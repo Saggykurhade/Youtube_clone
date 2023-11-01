ed1b27ed0d7222b14c6614da2217e073fa81f580
Saggykurhade <138295823+Saggykurhade@users.noreply.github.com>
2023-11-01 19:46:40 +0530

    updates

--- website/src/Components/Register.jsx
+++ website/src/Components/Register.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React from 'react';
+import './register.css'
 
 const Register = () => {
+
   return (
-    <div></div>
+    <div className='register-container'>
+
+      <div className='register-div'>
+        <div id='google-logo'>
+          <img src="images/google_logo.png" alt="" />
+        </div>
+
+        <h3>Create a Google Account</h3>
+        <p className='register-link'>Enter your name</p>
+
+        <div className='name-div'>
+          <input type='text' placeholder='First name' />
+        </div>
+        <div className='name-div'>
+          <input type='text' placeholder='Last name (optional)' />
+        </div>
+
+        <div id='next-div'>
+          <button>Next</button>
+        </div>
+      </div>
+
+      <div id='bottom-options'>
+        <select>
+          <option>English (United Kingdom)</option>
+          <option>Kiswahili</option>
+          <option>latviešu</option>
+          <option>lietuvių</option>
+          <option>Melayu</option>
+        </select>
+        <div className='help'>
+          <p>Help</p>
+          <p>Privacy</p>
+          <p>Terms</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
